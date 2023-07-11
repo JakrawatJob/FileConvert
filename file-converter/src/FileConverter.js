a3df556e4d1653fd1c5cc7b251d3efbc4ed597e2
Jakrawat\job_b <jakrawat@mm.co.th>
2023-07-11 17:13:00 +0700

    add display file

--- file-converter/src/FileConverter.js
+++ file-converter/src/FileConverter.js
@@ -3,10 +3,17 @@ import React, { useState } from 'react';
 const FileConverter = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [base64Data, setBase64Data] = useState('');
+  const [exampleDocumentUrl, setExampleDocumentUrl] = useState('');
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setExampleDocumentUrl(reader.result);
+    };
+    reader.readAsDataURL(file);
   };
 
   const convertToBase64 = () => {
@@ -22,12 +29,18 @@ const FileConverter = () => {
   return (
     <div>
       <h2>File Converter</h2>
-      <input type="file" accept=".pdf,.png" onChange={handleFileChange} />
+      <input type="file" accept=".pdf,.png,.jpg,.tiff,.tif" onChange={handleFileChange} />
       <button onClick={convertToBase64}>Convert to Base64</button>
       {base64Data && (
         <div>
           <h3>Base64 Data:</h3>
           <textarea value={base64Data} readOnly />
+        </div>
+      )}
+      {exampleDocumentUrl && (
+        <div>
+          <h3>Example Document:</h3>
+          <embed src={exampleDocumentUrl} type="application/pdf" width="100%" height="600px" />
         </div>
       )}
     </div>
